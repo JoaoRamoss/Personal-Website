eca4cf2ea4d0d57ad4474b9e1125f8546b38689d
João Silva <jprmsilva05@gmail.com>
2023-03-24 00:14:41 +0000

    Fixed typo in about section

--- my-website/src/Components/About/index.js
+++ my-website/src/Components/About/index.js
@@ -12,7 +12,7 @@ const About = () => {
             <SectionTitle>About me</SectionTitle>
             <SubTitle>Software Engineering Student at Universidade do Minho <br/>based in Fafe, Portugal 📍</SubTitle>
             <Content>
-            As a software engineering student, I am passionate about using technology to solve real-world problems and create innovative solutions. I am constantly seeking new challenges and opportunities to expand my skills and knowledge in the field.<br/> Outside of the classroom, I enjoy spending time with my friends and family, and explore new programming tools and frameworks.<br/> I'm excited about the opportunities that lie ahead, and I look forward to making a meaningful contribution to the industry.
+            As a software engineering student, I am passionate about using technology to solve real-world problems and create innovative solutions. I am constantly seeking new challenges and opportunities to expand my skills and knowledge in the field.<br/> Outside of the classroom, I enjoy spending time with my friends and family, and exploring new programming tools and frameworks.<br/> I'm excited about the opportunities that lie ahead, and I look forward to making a meaningful contribution to the industry.
             </Content>
           </Text>
         </AboutSection>
